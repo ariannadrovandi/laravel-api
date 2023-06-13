4181f2a47f887f66428bff551feb506ee9fc53f9
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-06-13 16:01:19 +0200

    milestone 1

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        return response()->json([
+            'success'=> true,
+            'results'=> $projects
+        ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::with('type', 'tags')->where('slug', $slug)->first();
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'results' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'Project not found !'
+            ]);
+        }
+
+
+    }
+}
